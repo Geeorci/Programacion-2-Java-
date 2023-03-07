9391a0bec47cfc413ec2255c164a657346059215
Geeorci <70101726+Geeorci@users.noreply.github.com>
2023-03-07 15:35:46 -0600

    tercer push

--- src/Casino.java
+++ src/Casino.java
@@ -1,5 +1,52 @@
+import java.util.Calendar;
+
 public class Casino {
     public static void main(String[] args) throws Exception {
-        System.out.println("esto es una prueva");
     }
+
+    public class User{
+        String name;
+        String lastName;
+        String lastNameII;
+        String Nickname;
+        String bird;
+        String email;
+        String photo;
+        char gender;
+        int id;
+    }
+
+    public class Wallet{
+        private float balance;
+        Card card;
+        int id;
+    
+        public void Wallet( int id ){
+            balance = 0;
+            this.id = id;
+        }
+        public void addBalance( Card card){
+            this.card = card;
+        }
+        public float getBalance(){
+            return balance;
+        }
+    }
+    public class Card{
+        String number;
+        String expire;
+        String name;
+        int type;
+        int id;
+
+        public Card(String number, String expire, String name, int type, int id ){
+            this.number = number;
+            this.expire = expire;
+            this.name = name;
+            this.type = type;
+            this.id = id;
+        }
+    }
+
+    public class Passworc{} 
 }
